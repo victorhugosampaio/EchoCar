ce62a7d1bcfc53572c63bf3554e0c8263201ba69
Victor Sampaio <victorhsl2828@gmail.com>
2025-01-02 23:22:21 -0300

    feat: Create the project base.
    Create the project first files.
    
    Signed-off-by: Victor Sampaio <victorhsl2828@gmail.com>

---
+++ src/main.c
@@ -0,0 +1,19 @@
+/**
+ * @file main.c
+ * @author victorhugosampaio
+ * @brief System main.
+ * @version 0.1
+ * @date 01-02-2025
+ *
+ * @copyright Copyright (c) 2025
+ *
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello World\n");
+
+    return 0;
+}
